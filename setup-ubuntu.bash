220530a675c1f56c5cfaec022c7728d97f02c12c
AJ ONeal <awesome@coolaj86.com>
2015-01-15 14:39:53 -0700

    Update setup-ubuntu.bash

--- setup-ubuntu.bash
+++ setup-ubuntu.bash
@@ -25,8 +25,6 @@ fi
 
 # iojs
 if [ -n "$(which node | grep node 2>/dev/null)" ]; then
-  IOJS_VER=""
-  
   if [ "$(node -v 2>/dev/null)" != "$(iojs -v 2>/dev/null)" ]; then
     echo "You have node.js installed. Backing up $(which node) as $(which node).joyent"
     echo "(you can move it back after the install if you want separate node.js and io.js installations)"
