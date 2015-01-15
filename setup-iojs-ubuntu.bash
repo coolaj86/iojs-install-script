0ff11fbf72d614ee2189b8ba08e46445fa7ad849
AJ ONeal <awesome@coolaj86.com>
2015-01-15 15:43:34 -0700

    no output for pushd

--- setup-iojs-ubuntu.bash
+++ setup-iojs-ubuntu.bash
@@ -8,9 +8,10 @@ fi
 
 if [ -n "${IOJS_VER}" ]; then
   echo "installing io.js as iojs ${IOJS_VER}..."
+
   curl -fsSL "http://iojs.org/dist/${IOJS_VER}/iojs-${IOJS_VER}-linux-${ARCH}.tar.gz" \
     -o "/tmp/iojs-${IOJS_VER}-linux-${ARCH}.tar.gz"
-  pushd /tmp
+  pushd /tmp >/dev/null
   tar xf /tmp/iojs-${IOJS_VER}-linux-${ARCH}.tar.gz
   rm iojs-${IOJS_VER}-linux-x64/{LICENSE,CHANGELOG.md,README.md}
   sudo rsync -a "/tmp/iojs-${IOJS_VER}-linux-${ARCH}/" /usr/local/
